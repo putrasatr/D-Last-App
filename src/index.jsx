3aa7913e79a58c1a6b6cebed92c5bf43fa01231a
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-10 16:25:13 +0700

    Jsx initial

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [valid, setvalid] = useState(null)
+    return (
+        <div>
+            <span>try JSX</span>
+        </div>
+    )
+}
+
+export default App;
